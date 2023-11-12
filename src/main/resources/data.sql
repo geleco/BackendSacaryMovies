5a07e82f924a507e47fbf272a523f9b67efaf3d7
geleco <rangel001@gmail.com>
2023-11-12 19:28:26 -0300

    atualizacao data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,3 +6,13 @@ CREATE TABLE IF NOT EXISTS movie (
 
 INSERT INTO movie (nome, url_imagem) VALUES ('A Bruxa de Blair', '/images/a_bruxa_de_blair.webp');
 INSERT INTO movie (nome, url_imagem) VALUES ('A Bruxa', '/images/a_bruxa.webp');
+INSERT INTO movie (nome, url_imagem) VALUES ('Arremate Para o Inferno', '/images/arremate_para_o_inferno.jpg');
+INSERT INTO movie (nome, url_imagem) VALUES ('Babadook', '/images/babadook.webp');
+INSERT INTO movie (nome, url_imagem) VALUES ('Corra!', '/images/corra.jpg');
+INSERT INTO movie (nome, url_imagem) VALUES ('Halloween', '/images/halloween.jpg');
+INSERT INTO movie (nome, url_imagem) VALUES ('Hereditário', '/images/hereditario.webp');
+INSERT INTO movie (nome, url_imagem) VALUES ('Invocação do mal', '/images/invocacao_do_mal.jpg');
+INSERT INTO movie (nome, url_imagem) VALUES ('O Bêbe de Rosimary', '/images/o_bebe_de_rosimary.jpeg');
+INSERT INTO movie (nome, url_imagem) VALUES ('O Brinquedo Assassino', '/images/o_brinquedo_assassino.jpg');
+INSERT INTO movie (nome, url_imagem) VALUES ('Halloween', '/images/halloween.jpg');
+INSERT INTO movie (nome, url_imagem) VALUES ('Halloween', '/images/halloween.jpg');
